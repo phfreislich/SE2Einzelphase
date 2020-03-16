a7a133f304193dffe350dd0c273305591e7d758e
Phil <phfreislich@edu.aau.at>
2020-03-16 12:30:08 +0100

    added Network-Client & GUI-IO

---
+++ app/src/main/java/com/freislich/se2einzelphase/Client.java
@@ -0,0 +1,46 @@
+package com.freislich.se2einzelphase;
+import java.net.*;
+import java.io.*;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.core.Observer;
+
+public class Client {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private Observer observer;
+    private String addr;
+    private int port;
+
+    Client(Observer observer){
+        this.observer=observer;
+        this.addr="se2-isys.aau.at";
+        this.port=53212;
+    }
+
+    public Observable<String> sendMessage(String message){
+        return Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> emitter) throws Throwable {
+                try {
+                    clientSocket = new Socket(addr, port);
+                    out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    out.println(message);
+                    String resp=in.readLine();
+                    in.close();
+                    out.close();
+                    clientSocket.close();
+                    observer.onNext(resp);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/freislich/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/freislich/se2einzelphase/MainActivity.java
@@ -1,14 +1,55 @@
 package com.freislich.se2einzelphase;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-
+    Client client;
+    EditText editText;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        client=new Client(myObserver);
+
+    }
+    private Observer myObserver = new Observer<String>() {
+        @Override
+        public void onSubscribe(@NonNull Disposable d) {
+
+        }
+
+        @Override
+        public void onNext(@NonNull String s) {
+            textView=findViewById(R.id.textView3);
+            textView.setText(s);
+        }
+
+        @Override
+        public void onError(@NonNull Throwable e) {
+
+        }
+
+        @Override
+        public void onComplete() {
+            System.out.println("I'm done!");
+        }
+    };
+    public void send(View view){
+        editText= (EditText) findViewById(R.id.editText);
+        Observable<String> myobs = client.sendMessage(editText.getText().toString());
+        myobs.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(myObserver);
     }
 }
